1e2e9315fb18d2c14be0f19d2d6afac49cfcc4af
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-06-14 08:36:28 +0530

    send definition to content script

--- background.js
+++ background.js
@@ -2,9 +2,37 @@ console.log('background running')
 
 browser.runtime.onMessage.addListener(receiver)
 
+function onError(error) {
+  console.error(`Error: ${error}`)
+}
+
 window.word = "coding train"
 
-function receiver(request, sender, sendResponse) {
+function receiver (request, sender, sendResponse) {
+  if (request.type !== 'wordSelection') return
   console.log(request)
   word = request.text
+
+  let url = `https://en.wiktionary.org/api/rest_v1/page/definition/${word}`
+  fetch(url, {
+    headers: new Headers({
+      'Api-User-Agent': 'notarama'
+    })
+  })
+    .then(data => data.json())
+    .then(data => {
+      console.log('<>', JSON.stringify(data, null, 2))
+
+      browser.tabs.sendMessage(
+        sender.tab.id,
+        {
+          type: 'definition',
+          definition: JSON.stringify(data.en[0].definitions[0].definition)
+        }
+      ).then(response => {
+        console.log("Message from the content script:")
+        console.log(response.response)
+      }).catch(onError)
+    },
+    err => console.log(err))
 }
--- content.js
+++ content.js
@@ -1,32 +1,28 @@
 window.addEventListener('mouseup', wordSelected)
 
+let selection = window.getSelection()
+
 function wordSelected() {
-  let selection = window.getSelection()
+  selection = window.getSelection()
   let selectedText = selection.toString().trim()
   console.log(selectedText, selection)
   if (selectedText.length > 0) {
     let message = {
+      type: 'wordSelection',
       text: selectedText
     }
     browser.runtime.sendMessage(message)
-
-    let def = document.createElement('h1')
-    def.style = 'background: cyan'
-
-    let url = `https://en.wiktionary.org/api/rest_v1/page/definition/selection`
-    url = `http://www.example.com`
-    fetch(url, {
-      mode: 'cors',
-      headers: new Headers({
-        'Api-User-Agent': 'notarama'
-      })
-    }).then(data => {
-      console.log(data)
-      def.innerHTML = data.en[0].definitions[0].definition
-      document.body.appendChild(def)
-    },
-    err => console.log(err))
-    def.innerHTML = 'Loading...'
-    document.body.appendChild(def)
   }
+}
+
+browser.runtime.onMessage.addListener(bubble)
+
+function bubble(data) {
+  console.log('bubble', data)
+  if (data.type !== 'definition') return
+  let def = document.createElement('h1')
+  def.style = 'background: cyan'
+  console.log(data)
+  def.innerHTML = data.definition
+  selection.anchorNode.parentNode.appendChild(def)
 }
