9307bf53fa35f9f41eff1c86ee3c74b74f0499dd
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-06-18 11:44:08 +0530

    fix bound()

--- content.js
+++ content.js
@@ -2,24 +2,48 @@ window.addEventListener('dblclick', wordSelected)
 window.addEventListener('mouseup', popPopup)
 let scrollBarWidth = window.innerWidth - document.getElementsByTagName('html')[0].clientWidth
 
+let defStyle = {
+  background: '#f4ecd8',
+  color: '#502020',
+  padding: '.5em',
+  fontSize: '1rem',
+  position: 'absolute',
+  width: '30em',
+  boxShadow: '1px 1px 5px black',
+  zIndex: '9999',
+  overflowWrap: 'break-word'
+}
+
+let wordStyle = {
+  fontSize: '1.2rem',
+  padding: '.2rem',
+  fontWeight: 'bold'
+}
+
+let moreStyle = {
+  padding: '.2em',
+  cssFloat: 'right'
+}
+
 let selection = window.getSelection()
 let popupHost = document.createElement('div')
+let loading = document.createElement('div')
+loading.innerText = 'Loading...'
+loading.style.backgroundColor = '#f4ecd8'
 let def = document.createElement('div')
-def.style.background = '#f4ecd8'
-def.style.color = '#502020'
-def.style.padding = '.5em'
-def.style.fontSize = '1rem'
-def.style.position = 'absolute'
-def.style.width = '30em'
-def.style.boxShadow = '1px 1px 5px black'
-def.style.zIndex = '9999'
-def.style.overflowWrap = 'break-word'
+Object.assign(def.style, defStyle)
+let word = document.createElement('div')
+Object.assign(word.style, wordStyle)
 let definition = document.createElement('div')
+let more = document.createElement('div')
+Object.assign(more.style, moreStyle)
+let anchor = document.createElement('a')
+anchor.setAttribute('target', '_blank')
+more.appendChild(anchor)
 document.body.appendChild(popupHost)
 popupHost.addEventListener('mouseup', e => e.stopPropagation())
 
 function wordSelected (event) {
-  console.log('target', event.target, event.currentTarget)
   selection = window.getSelection()
   let selectedText = selection.toString().trim()
   selectedText = selectedText.replace(/\s+/g, '_')
@@ -30,7 +54,10 @@ function wordSelected (event) {
       from: event.target === definition ? 'popup' : 'window'
     }
     browser.runtime.sendMessage(message)
-    def.innerHTML = '<div style="background-color: #f4ecd8;">Loading...</div>'
+    word.remove()
+    definition.remove()
+    more.remove()
+    def.appendChild(loading)
     popupHost.appendChild(def)
     if (event.target !== definition) {
       let range = selection.getRangeAt(0)
@@ -47,7 +74,7 @@ function popPopup () {
   let selectedText = selection.toString().trim()
   selectedText = selectedText.replace(/\s+/g, '_')
   if (selectedText.length === 0) {
-    popupHost.innerHTML = ''
+    def.remove()
   }
 }
 
@@ -55,27 +82,15 @@ browser.runtime.onMessage.addListener(bubble)
 
 function bubble (data) {
   if (data.type !== 'definition') return
-  let word = document.createElement('div')
-  word.style.fontSize = '1.2rem'
-  word.style.padding = '.2rem'
-  word.style.fontWeight = 'bold'
   word.innerText = data.word
   definition.innerHTML = data.definition
-  let more = document.createElement('div')
-  let anchor = document.createElement('a')
-  more.style.padding = '.2em'
-  more.style.cssFloat = 'right'
-  more.appendChild(anchor)
-  anchor.innerText = 'more>>>'
+  anchor.innerText = 'more >>>'
   anchor.setAttribute('href', data.url)
-  anchor.setAttribute('target', '_blank')
-  def.innerHTML = ''
+  loading.remove()
   def.appendChild(word)
   def.appendChild(definition)
-
   def.appendChild(more)
   popupHost.appendChild(def)
-  console.log(def.getBoundingClientRect())
   if (data.from === 'window') {
     bound(def)
   }
@@ -88,9 +103,9 @@ function bound (element) {
     element.style.left = -bodyRect.left + 'px'
   }
   if (rect.right > window.innerWidth) {
-    element.style.left = (window.innerWidth - rect.width - scrollBarWidth) + 'px'
+    element.style.left = (window.innerWidth - rect.width - bodyRect.left - scrollBarWidth) + 'px'
   }
   if (rect.bottom > window.innerHeight) {
-    element.style.top = (window.innerHeight - rect.height - scrollBarWidth) + 'px'
+    element.style.top = (window.innerHeight - rect.height - bodyRect.top - scrollBarWidth) + 'px'
   }
 }
